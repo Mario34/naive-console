5a6b345bce47014feca38c651577f7fac9e9d2ce
Mario34 <zhl.mario34@outlook.com>
2022-02-03 00:56:16 +0800

    fix: remove router guard permission to handle

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -1,4 +1,4 @@
-import { storageToken } from '@/storage'
+// import { storageToken } from '@/storage'
 // import { useUser } from '@/models/user'
 
 import type { Router } from 'vue-router'
@@ -6,30 +6,31 @@ import type { Router } from 'vue-router'
 export const guard = (router: Router) => {
   // const { state, getUserInfo, clearUserInfo } = useUser(router)
   router.beforeEach(async (to, form, next) => {
-    if (to.name === 'login' || to.name === 'login-password') {
-      if (!storageToken.get()) {
-        next()
-      } else {
-        // next('/')
-        next()
-      }
-    } else if (!storageToken.get()) {
-      // TODO: 添加重定向参数
-      next('/login')
-    } else {
-      // if (state.userInfo == null) {
-      //   try {
-      //     // TODO: 此处获取用户信息(权限)
-      //     await getUserInfo()
-      //   } catch {
-      //     clearUserInfo()
-      //     next('/login')
-      //     return
-      //   }
-      // }
-      // TODO: 权限校验
-      next()
-    }
+    next()
+    // if (to.name === 'login' || to.name === 'login-password') {
+    //   if (!storageToken.get()) {
+    //     next()
+    //   } else {
+    //     // next('/')
+    //     next()
+    //   }
+    // } else if (!storageToken.get()) {
+    //   // TODO: 添加重定向参数
+    //   next('/login')
+    // } else {
+    //   // if (state.userInfo == null) {
+    //   //   try {
+    //   //     // TODO: 此处获取用户信息(权限)
+    //   //     await getUserInfo()
+    //   //   } catch {
+    //   //     clearUserInfo()
+    //   //     next('/login')
+    //   //     return
+    //   //   }
+    //   // }
+    //   // TODO: 权限校验
+    //   next()
+    // }
   })
   router.beforeResolve((to, form, next) => {
     window.$appLoadingBar.start()
