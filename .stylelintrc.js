bcf5a34f57d8fc6f72b82ffda4ae1617bd9dc727
Mario34 <zhl.mario34@outlook.com>
2022-02-13 14:09:42 +0800

    chore(stylelint): upgrade stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  'extends': ['stylelint-config-standard'],
+  'extends': ['stylelint-config-standard-scss'],
   'plugins': [
     'stylelint-scss'
+  ],
+  'overrides': [
+    {
+      // sfc语法解析
+      files: ['*.vue', '**/*.vue'],
+      customSyntax: 'postcss-html',
+    },
   ],
   'rules': {
     'at-rule-no-unknown': null,
